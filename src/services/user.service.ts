ee10f82bd1da31c2e91cd0971e4f96a45616f7ec
Gastón Barrionuevo <gaston.cba97@gmail.com>
2023-12-19 20:50:27 -0300

    model.user added

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,39 +1,39 @@
-// import * as boom from "@hapi/boom";
-// import bcrypt from "bcrypt";
+import * as boom from "@hapi/boom";
+import bcrypt from "bcrypt";
 
-// import UserModel from "../models/user.model";
+import UserModel from "../models/user.model";
 
-// export class UserService {
-//   constructor() {}
+export class UserService {
+  constructor() {}
 
-//   async create(data: any) {
-//     const foundUser = await this.findByEmail(data.email);
+  async create(data: any) {
+    const foundUser = await this.findByEmail(data.email);
 
-//     if (foundUser) {
-//       throw boom.conflict(`email ${data.email} is repeated`);
-//     }
+    if (foundUser) {
+      throw boom.conflict(`email ${data.email} is repeated`);
+    }
 
-//     const hash = await bcrypt.hash(data.password, 10);
-//     const user = new UserModel({ ...data, password: hash });
-//     const savedUser = await (await user.save()).populate("brand");
-//     const userReturned = savedUser.toJSON();
-//     delete userReturned.password;
-//     return userReturned;
-//   }
+    const hash = await bcrypt.hash(data.password, 10);
+    const user = new UserModel({ ...data, password: hash });
+    const savedUser = await (await user.save()).populate("brand");
+    const userReturned = savedUser.toJSON();
+    delete userReturned.password;
+    return userReturned;
+  }
 
-//   async findByEmail(email: string) {
-//     return await UserModel.findOne({ email }).populate("brand");
-//   }
+  async findByEmail(email: string) {
+    return await UserModel.findOne({ email }).populate("brand");
+  }
 
-//   async findById(userId: string) {
-//     const user = await UserModel.findById(userId).populate("brand");
+  async findById(userId: string) {
+    const user = await UserModel.findById(userId).populate("brand");
 
-//     if (!user) {
-//       throw boom.notFound(`user #${userId} not found`);
-//     }
+    if (!user) {
+      throw boom.notFound(`user #${userId} not found`);
+    }
 
-//     const userJson = user.toJSON();
-//     delete userJson.password
-//     return userJson;
-//   }
-// }
+    const userJson = user.toJSON();
+    delete userJson.password
+    return userJson;
+  }
+}
